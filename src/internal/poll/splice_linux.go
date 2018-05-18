cc12663d64449630fd20b93b6fbe35543455a12d
Ben Burkert <ben@benburkert.com>
2018-05-17 23:06:15 -0700

    internal/poll: add pipe type for splice
    Add a pipe struct based on the pipe struct in HAProxy. Use a pipe to
    manage the underlying linux pipe for calls to splice.
    
    Change-Id: I09ac6671483bee5aa75047a39cf40f0b4b954d57

--- src/internal/poll/splice_linux.go
+++ src/internal/poll/splice_linux.go
@@ -1,7 +1,3 @@
-// Copyright 2018 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package poll
 
 import (
@@ -10,28 +6,22 @@ import (
 	"unsafe"
 )
 
-const (
-	// spliceNonblock makes calls to splice(2) non-blocking.
-	spliceNonblock = 0x2
-
-	// maxSpliceSize is the maximum amount of data Splice asks
-	// the kernel to move in a single call to splice(2).
-	maxSpliceSize = 4 << 20
-)
+const errEOF = syscall.Errno(0x0)
 
 // Splice transfers at most remain bytes of data from src to dst, using the
 // splice system call to minimize copies of data from and to userspace.
 //
-// Splice creates a temporary pipe, to serve as a buffer for the data transfer.
-// src and dst must both be stream-oriented sockets.
+// Splice creates a pipe to serve as a buffer for the data transfer. Both src
+// and dst must both be stream-oriented sockets.
 //
 // If err != nil, sc is the system call which caused the error.
 func Splice(dst, src *FD, remain int64) (written int64, handled bool, sc string, err error) {
-	prfd, pwfd, sc, err := newTempPipe()
+	pipe, sc, err := newPipe()
 	if err != nil {
 		return 0, false, sc, err
 	}
-	defer destroyTempPipe(prfd, pwfd)
+	defer pipe.release()
+
 	// From here on, the operation should be considered handled,
 	// even if Splice doesn't transfer any data.
 	if err := src.readLock(); err != nil {
@@ -48,134 +38,165 @@ func Splice(dst, src *FD, remain int64) (written int64, handled bool, sc string,
 	if err := dst.pd.prepareWrite(dst.isFile); err != nil {
 		return 0, true, "splice", err
 	}
-	var inPipe, n int
-	for err == nil && remain > 0 {
-		max := maxSpliceSize
-		if int64(max) > remain {
-			max = int(remain)
-		}
-		inPipe, err = spliceDrain(pwfd, src, max)
-		// spliceDrain should never return EAGAIN, so if err != nil,
-		// Splice cannot continue. If inPipe == 0 && err == nil,
-		// src is at EOF, and the transfer is complete.
-		if err != nil || (inPipe == 0 && err == nil) {
-			break
-		}
-		n, err = splicePump(dst, prfd, inPipe)
-		if n > 0 {
+
+	var dstEAGAIN, srcEAGAIN, seenEOF bool
+	for {
+		switch {
+		case seenEOF && pipe.data == 0:
+			// saw src EOF and pipe is empty, splice is finished
+
+			return written, true, "", nil
+		case !dstEAGAIN && pipe.data > 0:
+			// dst might be ready and pipe has data, try pumping data to dst
+
+			n, err := pipe.pumpTo(dst)
+			if err == syscall.EAGAIN {
+				dstEAGAIN = true
+				continue
+			}
+			if err != nil {
+				return written, true, "splice", err
+			}
+
 			written += int64(n)
 			remain -= int64(n)
+		case pipe.data == 0 && srcEAGAIN && !seenEOF:
+			// no pipe data and src would block, wait for src to be ready
+
+			if err := src.pd.waitRead(src.isFile); err != nil {
+				return written, true, "splice", err
+			}
+			srcEAGAIN = false
+		case pipe.data == 0 && !srcEAGAIN:
+			// no data and src might be ready, try draining from src
+
+			fallthrough
+		case !srcEAGAIN && !seenEOF && pipe.data < pipe.size:
+			// pipe has data but dst would block, also the pipe is not full and src might be ready, try draining from src
+
+			err := pipe.drainFrom(src, int(remain))
+			if err == syscall.EAGAIN {
+				srcEAGAIN = true
+				continue
+			}
+			if err == errEOF {
+				seenEOF = true
+				continue
+			}
+			if err != nil {
+				return written, true, "splice", err
+			}
+		case pipe.data >= pipe.size:
+			// pipe is full and dst would block, wait for dst to be ready
+
+			if err := dst.pd.waitWrite(dst.isFile); err != nil {
+				return written, true, "splice", err
+			}
+			dstEAGAIN = false
+		default:
+			// the pipe has data but is not full and both dst & src would
+			// block, and waiting on both src & dst is not possible, so wait on
+			// dst to be ready, and then assume src might be ready too
+
+			if err := dst.pd.waitWrite(dst.isFile); err != nil {
+				return written, true, "splice", err
+			}
+			dstEAGAIN, srcEAGAIN = false, false
 		}
 	}
+}
+
+type pipe struct {
+	fds  [2]int
+	data int
+	size int
+}
+
+var disableSplice unsafe.Pointer
+
+func newPipe() (*pipe, string, error) {
+	b := (*bool)(atomic.LoadPointer(&disableSplice))
+	if b != nil && *b {
+		return nil, "splice", syscall.EINVAL
+	} else if b == nil {
+		b = new(bool)
+		defer atomic.StorePointer(&disableSplice, unsafe.Pointer(b))
+	}
+
+	p := new(pipe)
+	if sc, err := p.alloc(); err != nil {
+		*b = false
+		return nil, sc, err
+	}
+	return p, "", nil
+}
+
+// drainFrom moves data from a sock to p.
+func (p *pipe) drainFrom(sock *FD, max int) error {
+	if free := p.size - p.data; max > free {
+		max = free
+	}
+
+	n, err := splice(p.fds[1], sock.Sysfd, max)
 	if err != nil {
-		return written, true, "splice", err
+		return err
 	}
-	return written, true, "", nil
+	if n == 0 {
+		return errEOF
+	}
+
+	p.data += n
+	return nil
 }
 
-// spliceDrain moves data from a socket to a pipe.
-//
-// Invariant: when entering spliceDrain, the pipe is empty. It is either in its
-// initial state, or splicePump has emptied it previously.
-//
-// Given this, spliceDrain can reasonably assume that the pipe is ready for
-// writing, so if splice returns EAGAIN, it must be because the socket is not
-// ready for reading.
-//
-// If spliceDrain returns (0, nil), src is at EOF.
-func spliceDrain(pipefd int, sock *FD, max int) (int, error) {
-	for {
-		n, err := splice(pipefd, sock.Sysfd, max, spliceNonblock)
-		if err != syscall.EAGAIN {
-			return n, err
-		}
-		if err := sock.pd.waitRead(sock.isFile); err != nil {
-			return n, err
-		}
+// pumpTo moves buffered data from p to sock.
+func (p *pipe) pumpTo(sock *FD) (int, error) {
+	n, err := splice(sock.Sysfd, p.fds[0], p.data)
+	if err != nil {
+		return n, err
 	}
+
+	p.data -= n
+	return n, nil
 }
 
-// splicePump moves all the buffered data from a pipe to a socket.
-//
-// Invariant: when entering splicePump, there are exactly inPipe
-// bytes of data in the pipe, from a previous call to spliceDrain.
-//
-// By analogy to the condition from spliceDrain, splicePump
-// only needs to poll the socket for readiness, if splice returns
-// EAGAIN.
-//
-// If splicePump cannot move all the data in a single call to
-// splice(2), it loops over the buffered data until it has written
-// all of it to the socket. This behavior is similar to the Write
-// step of an io.Copy in userspace.
-func splicePump(sock *FD, pipefd int, inPipe int) (int, error) {
-	written := 0
-	for inPipe > 0 {
-		n, err := splice(sock.Sysfd, pipefd, inPipe, spliceNonblock)
-		// Here, the condition n == 0 && err == nil should never be
-		// observed, since Splice controls the write side of the pipe.
-		if n > 0 {
-			inPipe -= n
-			written += n
-			continue
-		}
-		if err != syscall.EAGAIN {
-			return written, err
-		}
-		if err := sock.pd.waitWrite(sock.isFile); err != nil {
-			return written, err
-		}
-	}
-	return written, nil
-}
+const (
+	// spliceNonblock makes calls to splice(2) non-blocking.
+	spliceNonblock = 0x2
+)
 
 // splice wraps the splice system call. Since the current implementation
 // only uses splice on sockets and pipes, the offset arguments are unused.
 // splice returns int instead of int64, because callers never ask it to
 // move more data in a single call than can fit in an int32.
-func splice(out int, in int, max int, flags int) (int, error) {
-	n, err := syscall.Splice(in, nil, out, nil, max, flags)
+func splice(out, in, max int) (int, error) {
+	n, err := syscall.Splice(in, nil, out, nil, max, spliceNonblock)
 	return int(n), err
 }
 
-var disableSplice unsafe.Pointer
-
-// newTempPipe sets up a temporary pipe for a splice operation.
-func newTempPipe() (prfd, pwfd int, sc string, err error) {
-	p := (*bool)(atomic.LoadPointer(&disableSplice))
-	if p != nil && *p {
-		return -1, -1, "splice", syscall.EINVAL
-	}
-
-	var fds [2]int
+func (p *pipe) alloc() (string, error) {
 	// pipe2 was added in 2.6.27 and our minimum requirement is 2.6.23, so it
 	// might not be implemented. Falling back to pipe is possible, but prior to
-	// 2.6.29 splice returns -EAGAIN instead of 0 when the connection is
-	// closed.
+	// 2.6.27 splice returns EAGAIN instead of EOF when the connect is closed.
 	const flags = syscall.O_CLOEXEC | syscall.O_NONBLOCK
-	if err := syscall.Pipe2(fds[:], flags); err != nil {
-		return -1, -1, "pipe2", err
+	if err := syscall.Pipe2(p.fds[:], flags); err != nil {
+		return "pipe2", err
 	}
 
-	if p == nil {
-		p = new(bool)
-		defer atomic.StorePointer(&disableSplice, unsafe.Pointer(p))
-
-		// F_GETPIPE_SZ was added in 2.6.35, which does not have the -EAGAIN bug.
-		if _, _, errno := syscall.Syscall(syscall.SYS_FCNTL, uintptr(fds[0]), syscall.F_GETPIPE_SZ, 0); errno != 0 {
-			*p = true
-			destroyTempPipe(fds[0], fds[1])
-			return -1, -1, "fcntl", errno
-		}
+	// F_GETPIPE_SZ was added in 2.6.35, which does not have the -EAGAIN bug.
+	size, _, errno := syscall.Syscall(syscall.SYS_FCNTL, uintptr(p.fds[0]), syscall.F_GETPIPE_SZ, 0)
+	if errno != 0 {
+		p.release()
+		return "fcntl", errno
 	}
+	p.size = int(size)
 
-	return fds[0], fds[1], "", nil
+	return "", nil
 }
 
-// destroyTempPipe destroys a temporary pipe.
-func destroyTempPipe(prfd, pwfd int) error {
-	err := CloseFunc(prfd)
-	err1 := CloseFunc(pwfd)
+func (p *pipe) release() error {
+	err := CloseFunc(p.fds[0])
+	err1 := CloseFunc(p.fds[1])
 	if err == nil {
 		return err1
 	}
